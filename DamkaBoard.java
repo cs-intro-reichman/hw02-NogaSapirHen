5dc60addece731b25b57c3ff37e87b928aad1293
NogaSapirHen <noga.sapirhen@post.runi.ac.il>
2024-01-04 16:00:13 +0200

    4 try

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -7,16 +7,14 @@ public class DamkaBoard
 	for ( int i = 0; i < n; i++ )
 	{
 		// every second row starts with space
-		if ( i % 2 == 1 )
-			System.out.print ( " * " );
 		if ( i % 2 == 0 )
-			System.out.print ( "*" );
+			System.out.print ( " *" );
 		for ( int j = 0; j < n - 1; j++ )
 		{
 			System.out.print ( "* " );
 		}
-		if ( i % 2 == 0 )
-			System.out.print ( "*" );
+		if ( i % 2 == 1 )
+			System.out.print ( " * " );
 		// next row
 		System.out.println (" ");
 	}
