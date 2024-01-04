c180fb71e50c39ab83e9d0860af6546b783d1681
NogaSapirHen <noga.sapirhen@post.runi.ac.il>
2024-01-04 17:27:18 +0200

    damka again

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -9,17 +9,18 @@ public class DamkaBoard
 		if ( i % 2 == 0 )
 		{
 			System.out.print ( "*" );
-		}
+		
+
 		// every second row starts with space
 		for ( int j = 0; j < n-1; j++ )
 		{
 			System.out.print ( " *" );
-			if ( i % 2 == 1 )
+		}
+		// next row
+		if ( i % 2 == 1 )
 			System.out.print ( " *" );
 			else 
 			System.out.print ( " " );
-		}
-		// next row
 		System.out.println ( );
 	}
 	}
