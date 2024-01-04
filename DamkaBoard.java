e052b933ab816826be6c436e6900e920f9674fdd
NogaSapirHen <noga.sapirhen@post.runi.ac.il>
2024-01-04 16:27:50 +0200

    newtry

--- DamkaBoard.java
+++ DamkaBoard.java
@@ -6,14 +6,16 @@ public class DamkaBoard
 	int n = Integer.parseInt ( args[0] );
 	for ( int i = 0; i < n; i++ )
 	{
+		if ( i % 2 == 0 )
+			System.out.print ( "*" );
 		// every second row starts with space
-		for ( int j = 0; j < n; j++ )
+		for ( int j = 0; j < n-1; j++ )
 		{
-			if ( i % 2 == 0 )
 			System.out.print ( " *" );
-			//System.out.print ( " *" );
 			if ( i % 2 == 1 )
-			System.out.print ( "* " );
+			System.out.print ( " *" );
+			else 
+			System.out.print ( " " );
 		}
 		// next row
 		System.out.println (" ");
---
+++ OneOfEachStats.java
@@ -0,0 +1,52 @@
+public class OneOfEachStats
+{
+    public static void main(String[] args) 
+	{
+	// Declares integer
+	int T = Integer.parseInt(args[0]);
+		int seed = Integer.parseInt(args[1]);
+		// Initailizes a random numbers generator with the given seed value
+		  Random generator = new Random(seed);  
+	int family2 = 0;
+	int family3 = 0;
+	int family4orMore = 0;
+	double average = 0;
+	int mostCommon = 0;
+	// for each family
+	for ( int i = 0; i < T; i ++ )
+	{
+	// each family
+	boolean isGirl = false;
+	boolean isBoy = false;
+	int count = 0;
+	while ( !isBoy || !isGirl )
+	{
+		// the int will be 1 or 0
+	double rnd = generator.nextDouble();
+	isGirl = ( rnd < 0.5 );
+	isBoy = (rnd > 0.5 );
+	count ++;
+	}
+	// adds to the general counts
+	if ( count == 2 )
+		family2 ++;
+	if ( count == 3 )
+		family3 ++;
+	if ( count >= 4 )
+		family4orMore ++;
+	average += count; 
+	}
+	average = average / T ;
+	System.out.println ( "Average: " + average + " children to get at least one of each gender." );
+	System.out.println ( "Number of families with 2 children: " + family2 );
+	System.out.println ( "Number of families with 3 children: " + family3 );
+	System.out.println ( "Number of families with 4 or more children: " + family4orMore );
+	mostCommon = Math.max ( family2, Math.max ( family3, family4orMore ) );
+	if ( mostCommon == family2 )
+		System.out.println ( "The most common number of children is 2." );
+	else if ( mostCommon == family3 )
+		System.out.println ( "The most common number of children is 3." );
+	else
+		System.out.println ( "The most common number of children is 4 or more." );
+}
+}
